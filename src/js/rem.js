b6fd1d06a822b79f4bbea0c8453e645bd1261508
JASPER-LY <31533224+JASPER-LY@users.noreply.github.com>
2018-01-18 17:01:54 +0800

    Update rem.js

--- src/js/rem.js
+++ src/js/rem.js
@@ -1,5 +1,4 @@
 (function (doc, win) {
-<<<<<<< HEAD
         var docEl = doc.documentElement,
             resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
             recalc = function () {
@@ -17,24 +16,5 @@
         win.addEventListener(resizeEvt, recalc, false);
        recalc();
     })(document, window);
-=======
-    var docEl = doc.documentElement,
-        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-        recalc = function () {
-            var clientWidth = docEl.clientWidth;
-            if (!clientWidth) return;
-            if(clientWidth>=750){
-                // 这里的640 取决于设计稿的宽度
-                docEl.style.fontSize = '100px';
-            }else{
-                docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
-            }
-        };
-    
-    if (!doc.addEventListener) return;
-    win.addEventListener(resizeEvt, recalc, false);
-    recalc();
-})(document, window);
->>>>>>> f32b1b269d7c50130e8e28ba5c940208ceda8ae7
-
+ 
 
